b445e7eb1a6d8103497b9e281055c3eb367cc3df
mgelios <antonnadezhkin@gmail.com>
2020-01-03 19:35:25 +0300

    added fire animation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-
 #define DATA_PIN    4
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
@@ -9,17 +8,20 @@
 CRGB leds[112];
 int counter = 0;
 int NUMBER_OF_LEDS = 112;
+short fire_energy[112];
+CRGB fire_colors[8] = {0x000000, 0x3F0000, 0x7F0000, 0xFF0000, 0xFF3F00, 0xFF7F00, 0xFFBF00, 0xFFFF00};
 
 void setup() {
   delay(1000);
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUMBER_OF_LEDS).setCorrection(TypicalSMD5050);
   FastLED.setBrightness(127);
+  memset(fire_energy, 0, sizeof(fire_energy));
 }
 
 void colorBlinkLoopStep() {
   for (int i = 0; i < NUMBER_OF_LEDS; i++) {
     leds[i].fadeToBlackBy(64);
-    if (random(10) == 1) {
+    if (random(16) == 1) {
       leds[i].setHSV(random(256), 255,255);
     }
   }
@@ -28,7 +30,7 @@ void colorBlinkLoopStep() {
 void whiteBlinkLoopStep() {
   for (int i = 0; i < NUMBER_OF_LEDS; i++) {
     leds[i].fadeToBlackBy(64);
-    if (random(16) == 1) {
+    if (random(32) == 1) {
       leds[i].setRGB(255, 255, 255);
     }
   }
@@ -51,6 +53,18 @@ void smallHSVLoopStep() {
   }
 }
 
+void fireAnimationLoopStep() {
+  for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+    if (fire_energy[i] == 0) {
+      fire_energy[i] = random(8);
+    } else {
+      fire_energy[i] -= 1;
+    }
+
+    leds[i] = fire_colors[fire_energy[i]];
+  }
+}
+
 void loop() {
   while (true) {
     if (counter > 255) {
@@ -59,7 +73,9 @@ void loop() {
 
     //colorBlinkLoopStep();
     //globalHSVLoopStep();
-    smallHSVLoopStep();
+    //smallHSVLoopStep();
+    //whiteBlinkLoopStep();
+    fireAnimationLoopStep();
 
     FastLED.show();
     delay(100);
